7cb59b943ec144d692f54074a15e53dc59c80e49
Astrokit113 <pasyaandraka@hotmail.com>
2026-03-26 06:12:56 +0700

    Third Commit

---
+++ blog/.eleventy.js
@@ -0,0 +1,32 @@
+const htmlmin = require("html-minifier-terser");
+
+module.exports = function (eleventyConfig) {
+
+  // 1. Blog content is passed through (no separate CSS/images needed)
+
+  // 2. Custom permalink structure
+  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  // 3. Minify HTML (Moved above the return statement so it actually runs!)
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  // 4. Return the folder configuration at the VERY END
+  return {
+    dir: {
+      input: "content",
+      output: "../public/blog",
+    },
+  };
+};
---
+++ blog/content/blog/first-blog.md
@@ -0,0 +1,10 @@
+---
+layout: myTemplate.njk
+title: My Thoughts on Electric Cars
+---
+
+Here is my brand new blog post about electric vehicles. 
+
+* They are quiet.
+* They are fast.
+* I don't have to write any HTML list tags to make these bullet points, because 11ty converts my Markdown automat
