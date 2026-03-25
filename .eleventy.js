99a7bc17cf86b9ed165b860e88614fd5683b61bc
Astrokit113 <pasyaandraka@hotmail.com>
2026-03-25 22:25:58 +0700

    First Major Commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,30 @@
+const htmlmin = require("html-minifier-terser");
+
 module.exports = function (eleventyConfig) {
 
-  // This will stop the default behaviour of foo.html being turned into foo/index.html
+  // 1. Tell 11ty to copy your CSS and Images to the public folder
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("images");
+
+  // 2. Custom permalink structure
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
 
+  // 3. Minify HTML (Moved above the return statement so it actually runs!)
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  // 4. Return the folder configuration at the VERY END
   return {
     dir: {
       input: "content",
---
+++ content/index.md
@@ -0,0 +1,8 @@
+---
+layout: myTemplate.njk
+title: Welcome to My Neocities Site!
+---
+
+## Hello World!
+
+This is my brand new homepage. The server is working perfectly!
